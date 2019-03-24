8d19e135fa71a556af209846ec2e4f5529006a19
absognety <vikasch.1994@gmail.com>
2019-03-24 11:03:50 +0530

    conditional expressions - scala

---
+++ Conditional_Expressions/problem02.scala
@@ -0,0 +1,21 @@
+package Conditional_Expressions
+
+/**
+  * Created by cvikas10 on 3/24/2019.
+  */
+object problem02 {
+  def main(args: Array[String]): Unit = {
+    val a:Int = 1
+    val b:Int = 5
+    if (a < 2){
+      println("a is less than 2")
+    } else {
+      println("a is not less than 2")
+    }
+    if (b < 2){
+      println("b is less than 2")
+    } else {
+      println("b is not less than 2")
+    }
+  }
+}
