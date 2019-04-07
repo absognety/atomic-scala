776b4af6fa30336c8cf7a7cba1891dc847d0dd4d
absognety <vikasch.1994@gmail.com>
2019-04-07 14:10:17 +0530

    added println in play method, print(cell.entry)

--- src/main/scala/Summary_II/problem07.scala
+++ src/main/scala/Summary_II/problem07.scala
@@ -21,15 +21,19 @@ object problem07 {
         Vector(new Cell,new Cell,new Cell)
       )
       def play(e:Char,x:Int,y:Int): String={
-        if (x<0 || x>2 || y<0 || y>2)
-          "Invalid Move"
-        else
-          cells(x)(y).set(e)
+        val status = {
+          if (x<0 || x>2 || y<0 || y>2)
+            "Invalid Move"
+          else
+            cells(x)(y).set(e)
+        }
+        displayBoard()
+        status
       }
       def displayBoard(): Unit={
         for (m <- cells) {
           for (n <- m)
-            println(n)
+            print(n.entry + " ")
         }
       }
     }
@@ -37,6 +41,23 @@ object problem07 {
     println(grid.play('X',1,1))
     println(grid.play('X',1,1))
     println(grid.play('O',1,3))
+    grid.play('O', 0, 0)
+    grid.play('X', 2, 2)
     grid.displayBoard()
   }
 }
+/* OUTPUT_SHOULD_BE
+  X
+successful move
+  X
+invalid move
+  X
+invalid move
+O
+  X
+successful move
+O
+  X
+    X
+successful move
+*/
