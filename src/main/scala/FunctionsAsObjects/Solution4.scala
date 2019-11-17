3353f96aa502bab95c67f746e415e6d2d524a92a
absognety <vikasch.1994@gmail.com>
2019-11-17 21:52:22 +0530

    solution 4 fill function in foreach

---
+++ src/main/scala/FunctionsAsObjects/Solution4.scala
@@ -0,0 +1,10 @@
+package FunctionsAsObjects
+
+object Solution4 {
+  def main(args: Array[String]): Unit = {
+    var s = ""
+    val v = Vector(1, 5, 7, 8)
+    v.foreach(n => s = s + (n*7).toString + " ")
+    println(s)
+  }
+}
