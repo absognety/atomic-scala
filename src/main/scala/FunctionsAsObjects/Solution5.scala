95921c7ddf9c38f8f2bc2b5b54798309fdaacf10
absognety <vikasch.1994@gmail.com>
2019-11-17 21:59:57 +0530

    solution 4,5 fixed up with correct implementations

--- src/main/scala/FunctionsAsObjects/Solution5.scala
+++ src/main/scala/FunctionsAsObjects/Solution5.scala
@@ -1,11 +1,10 @@
 package FunctionsAsObjects
 
-object Solution4 {
+object Solution5 {
   def main(args: Array[String]): Unit = {
     var s = ""
     val v = Vector(1, 5, 7, 8)
-    val dogYears = (x:Int) => x*7
-    v.foreach(n => s = s + dogYears(n).toString + " ")
+    v.foreach(n => s = s + (n*7).toString + " ")
     println(s)
   }
 }
