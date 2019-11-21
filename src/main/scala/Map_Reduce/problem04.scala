c3486ad0d6f241e6a7b4ec445b43cb8e1b94b528
absognety <vikasch.1994@gmail.com>
2019-11-21 12:11:00 +0530

    for loop instead of map for SimpleMap.scala

---
+++ src/main/scala/Map_Reduce/problem04.scala
@@ -0,0 +1,11 @@
+package Map_Reduce
+
+object problem04 {
+  def main(args: Array[String]): Unit = {
+    val v = Vector(1, 2, 3, 4)
+    for (i <- v) {
+      println(i)
+    }
+    //Rewrite simpleMap.scala using for in place of map
+  }
+}
