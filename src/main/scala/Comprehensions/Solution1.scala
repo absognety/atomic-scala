3c24d6b161e62d850edc32c08dd5414f1dc725b9
absognety <vikasch.1994@gmail.com>
2019-11-21 15:15:00 +0530

    change name: yielding to name: generator

---
+++ src/main/scala/Comprehensions/Solution1.scala
@@ -0,0 +1,10 @@
+package Comprehensions
+
+object Solution1 {
+  def main(args: Array[String]): Unit = {
+    /*
+    change yielding to more descriptive name
+    i.e generator which generates an iterable
+     */
+  }
+}
