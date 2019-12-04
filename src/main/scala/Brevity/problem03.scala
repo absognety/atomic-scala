aece4e45be4a54b1490c27fdbdac565a733d62b4
absognety <vikasch.1994@gmail.com>
2019-12-04 14:58:38 +0530

    03. remove return type for brevity

--- src/main/scala/Brevity/problem03.scala
+++ src/main/scala/Brevity/problem03.scala
@@ -2,6 +2,14 @@ package Brevity
 
 object problem03 {
   def main(args: Array[String]): Unit = {
-
+    def assignResult(arg:Boolean):Int = {
+      //intermediate variable result is being used here.
+      val result = if(arg) 42 else 47
+      result
+    }
+    println(assignResult(true))
+    def assignResult3(arg:Boolean) = if(arg) 42 else 47
+    println(assignResult3(true))
+    println(assignResult3(false))
   }
 }
