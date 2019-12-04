8887acfb46a944bab36c708806ffcf62600e2072
absognety <vikasch.1994@gmail.com>
2019-12-04 12:37:00 +0530

    sbt clean compile

--- Hello.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by cvikas10 on 2/9/2019.
-  */
-object Hello extends App{
-  println("Hello World!!!")
-}
---
+++ src/main/scala/Brevity/problem03.scala
@@ -0,0 +1,7 @@
+package Brevity
+
+object problem03 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
