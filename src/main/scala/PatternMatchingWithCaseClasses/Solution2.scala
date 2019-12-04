652eb5e9a3dbe7ef3ea56e136ce06707bc97f50e
absognety <vikasch.1994@gmail.com>
2019-12-04 11:07:19 +0530

    extract object from inputs - solution 2

---
+++ src/main/scala/PatternMatchingWithCaseClasses/Solution2.scala
@@ -0,0 +1,34 @@
+package PatternMatchingWithCaseClasses
+
+object Solution2 {
+  def main(args: Array[String]): Unit = {
+    case class Passenger(first:String, last:String)
+    case class Train(travelers:Vector[Passenger],
+                     line:String)
+    case class Bus(passengers:Vector[Passenger],
+                   capacity:Int)
+    case class Plane(passengers:Vector[Passenger],
+                     planeName:String)
+    def travel(transport:Any):String = {
+      transport match {
+        case Train(travelers, line) =>
+          s"Train line $line $travelers"
+        case Bus(travelers, seats) =>
+          s"Bus size $seats $travelers"
+        case Plane(travelers,planeName) =>
+          s"Plane $planeName + $travelers"
+        case Passenger(first,last) => s"$first is walking"
+        case what => s"$what is in limbo!"
+      }
+    }
+    val travelers = Vector(
+      Passenger("Harvey", "Rabbit"),
+      Passenger("Dorothy", "Gale"))
+    val trip2 = Vector(
+      Train(travelers, "Reading"),
+      Plane(travelers, "B757"),
+      Bus(travelers, 100),
+      Passenger("Sally","Marie"))
+    println(travel(trip2(3)))
+  }
+}
