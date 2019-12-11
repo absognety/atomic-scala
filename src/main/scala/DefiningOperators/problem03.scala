0d06092fc482833b582ba12099f11736e40fc1f9
absognety <vikasch.1994@gmail.com>
2019-12-11 13:31:18 +0530

    replace power with ^ operator

---
+++ src/main/scala/DefiningOperators/problem03.scala
@@ -0,0 +1,15 @@
+package DefiningOperators
+import scala.math.pow
+
+object problem03 {
+  def main(args: Array[String]): Unit = {
+    class FancyNumber1(num:Int) {
+      val number = num
+      def ^(N:Int) = {pow(num,N)}
+    }
+    val fancyNumber = new FancyNumber1(2)
+    println(fancyNumber.^(3).toLong)
+    val b1 = new FancyNumber1(10)
+    println(b1.^(2).toLong)
+  }
+}
