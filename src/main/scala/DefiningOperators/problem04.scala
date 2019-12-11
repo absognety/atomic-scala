fbc29e99e2b3c3e942e26ea195c967cc0e2d6d62
absognety <vikasch.1994@gmail.com>
2019-12-11 13:35:06 +0530

    replace .func with just func and make it more readable with ease

--- src/main/scala/DefiningOperators/problem04.scala
+++ src/main/scala/DefiningOperators/problem04.scala
@@ -1,15 +1,15 @@
 package DefiningOperators
 import scala.math.pow
 
-object problem03 {
+object problem04 {
   def main(args: Array[String]): Unit = {
     class FancyNumber1(num:Int) {
       val number = num
-      def ^(N:Int) = {pow(num,N)}
+      def **(N:Int) = {pow(num,N)}
     }
     val fancyNumber = new FancyNumber1(2)
-    println(fancyNumber.^(3).toLong)
+    println(fancyNumber.**(3).toLong)
     val b1 = new FancyNumber1(10)
-    println((b1 ^ 2).toLong)
+    println((b1**2).toLong)
   }
 }
