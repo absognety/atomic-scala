00b8a6902a93eb1d0b514c635e3d955a27c62988
absognety <vikasch.1994@gmail.com>
2019-12-18 15:08:43 +0530

    unpacking into a tuple

---
+++ src/main/scala/Tuples/Solution1.scala
@@ -0,0 +1,11 @@
+package Tuples
+
+object Solution1 {
+  def main(args: Array[String]): Unit = {
+    val tuple1 = (65, "Sunny", "Stars")
+    val (temp1,sky1,view1) = tuple1
+    println(temp1)
+    println(sky1)
+    println(view1)
+  }
+}
