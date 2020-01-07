2dbf51a625837f5a27f895b968881b1e012048aa
absognety <vikasch.1994@gmail.com>
2020-01-07 08:51:25 +0530

    ex5: playing around with abstract classes

---
+++ src/main/scala/AbstractClasses/solution05.scala
@@ -0,0 +1,24 @@
+package AbstractClasses
+
+object solution05 {
+  abstract class Animal {
+    def templateMethod =
+      s"The $animalName goes $sound"
+    // Abstract methods (no method body):
+    def animalName:String
+    def animal(b:Int):String
+    def sound:String
+  }
+  class Dog extends Animal {
+    def animalName="Dog"
+    override def animal(x:Int) = {s"dog has $x puppies"}
+    def sound = "Bow!"
+  }
+
+  def main(args: Array[String]): Unit = {
+    println((new Dog).animal(5))
+    println((new Dog).animal(2))
+    println((new Dog).sound)
+    println((new Dog).templateMethod)
+  }
+}
