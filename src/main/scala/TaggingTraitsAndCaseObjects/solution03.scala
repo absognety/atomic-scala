d27df2522fd7828e5bfd38cce0e38ed138c8a6aa
absognety <vikasch.1994@gmail.com>
2020-01-25 20:16:36 +0530

    ex3: Adding another Red to EnumColor2

---
+++ src/main/scala/TaggingTraitsAndCaseObjects/solution03.scala
@@ -0,0 +1,20 @@
+package TaggingTraitsAndCaseObjects
+
+object EnumColor2 extends Enumeration {
+  type EnumColor2 = Value
+  val Red = Value("Red")
+  val Blue = Value("Blue")
+  val Green = Value("Green")
+  val Red2 = Value("Red")
+}
+object solution03 {
+  def main(args: Array[String]): Unit = {
+    println (EnumColor2.Red)
+    println (EnumColor2.Blue)
+    println (EnumColor2.Green)
+    println (EnumColor2.Red2)
+  }
+}
+/*
+As long as the enumeration name is different, can add duplicate colors
+ */
