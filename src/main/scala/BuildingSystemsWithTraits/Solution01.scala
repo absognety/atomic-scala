42b2f09b183a81dae94f3eadf8a62c89ec773def
absognety <vikasch.1994@gmail.com>
2020-01-28 16:10:36 +0530

    modify Coffee.scala with traits to satisfy tests shown

--- src/main/scala/BuildingSystemsWithTraits/Solution01.scala
+++ src/main/scala/BuildingSystemsWithTraits/Solution01.scala
@@ -1,51 +1,55 @@
 package BuildingSystemsWithTraits
 
 object Solution01 {
-  trait Coffee {
-    var shots = 2
-    var decaf = false
-    var milk = false
-    var toGo = false
-    var syrup = ""
-    var result = ""
-    println(shots,decaf,milk,toGo,syrup)
-    def getCup = {
-      if(toGo)
-        result += "ToGoCup "
-      else
-        result += "HereCup "
-    }
-    def pourShots = {
-      for(s <- 0 until shots)
-        if(decaf)
-          result += "decaf shot "
-        else
-          result += "shot "
-    }
-    def addMilk = {
-      if(milk)
-        result += "milk "
-    }
-    def addSyrup = {
-      result += syrup
-    }
+  trait shots
+  trait decaf
+  trait milk
+  trait toGo
+  trait syrup
+  case class Coffee(Shots:shots,
+                    deCaf:decaf,
+                    Milk:milk,
+                    ToGo:toGo,
+                    Syrup:syrup) {
+    val numofshots = Shots.toString
+    val typeOfdecaf = deCaf.toString
+    val isMilk = Milk.toString
+    val isToGo = ToGo.toString
+    val isSyrup = Syrup.toString
+    override def toString: String = s"Coffee($numofshots,$typeOfdecaf,$isMilk,$isToGo,$isSyrup)"
   }
-  class Main extends Coffee
   def main(args: Array[String]): Unit = {
-    val usual = new Main
-    usual.getCup
-    usual.pourShots
-    usual.addMilk
-    usual.addSyrup
-    println(usual.result)
-    val mocha = new Main
-    mocha.decaf = true
-    mocha.toGo = true
-    mocha.syrup = "Chocolate"
-    mocha.getCup
-    mocha.pourShots
-    mocha.addMilk
-    mocha.addSyrup
-    println(mocha.result)
+    class Single extends shots {
+      override def toString: String = "Single"}
+    class Caf extends decaf {
+      override def toString: String = "Caf"
+    }
+    class HalfCaf extends decaf {
+      override def toString: String = "HalfCaf"
+    }
+    class Here extends milk {
+      override def toString: String = "Here"
+    }
+    class Skim extends toGo {
+      override def toString: String = "Skim"
+    }
+    class ToGo extends milk {
+      override def toString: String = "ToGo"
+    }
+    class Choc extends syrup {
+      override def toString: String = "Choc"
+    }
+    class Double extends shots {
+      override def toString: String = "Double"
+    }
+    class NoMilk extends toGo {
+      override def toString: String = "NoMilk"
+    }
+    class NoFlavor extends syrup {
+      override def toString: String = "NoFlavor"
+    }
+    println (Coffee(new Single,new Caf,new Here,new Skim,new Choc))
+    println (Coffee(new Double,new Caf,new Here,new NoMilk,new NoFlavor))
+    println (Coffee(new Double,new HalfCaf,new ToGo,new Skim,new Choc))
   }
 }
