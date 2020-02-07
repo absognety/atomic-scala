e4b710b9bd2eddca28eda9362134fbd2be8255c6
absognety <vikasch.1994@gmail.com>
2020-02-07 11:22:21 +0530

    ex3: change the contents/elements of a immutable vector by using map

---
+++ src/main/scala/ReferencesAndMutability/Solution03.scala
@@ -0,0 +1,17 @@
+package ReferencesAndMutability
+
+import scala.math.pow
+object Solution03 {
+  val testv = Vector(1,2,3,4,5,6)
+
+  def main(args: Array[String]): Unit = {
+    println (testv.length)
+    val newtestv = testv.map(x => pow(x,2).round)
+    println ("new testv ",newtestv)
+    println ("Old testv",testv)
+    /*newtestv(3) = 45, this operation not permitted
+    by using functions like map,collect,flatten the elements of
+    a vector can be updated
+     */
+  }
+}
