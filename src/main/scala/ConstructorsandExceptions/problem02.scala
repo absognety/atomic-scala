1922ec2a69b0a5d1549978f8e28c8bd0c0edb996
absognety <vikasch.1994@gmail.com>
2020-02-12 19:10:14 +0530

    Adding line numbers to each line of a Vector from ex1

---
+++ src/main/scala/ConstructorsandExceptions/problem02.scala
@@ -0,0 +1,13 @@
+package ConstructorsandExceptions
+
+import codelisting.ExtensionException
+import codelisting.CodeListing._
+import codelisting.CodeListing
+object problem02 {
+  def main(args: Array[String]): Unit = {
+    val reader = new CodeListing("/home/vikas/atomic-scala/src/main/scala/ConstructorsandExceptions/codelisting/CodeListing.scala")
+    val vlen = reader.vec.length
+    println (vlen)
+    ((1 to vlen).zip(reader.vec)).foreach(println)
+  }
+}
