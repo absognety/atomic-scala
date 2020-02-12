4f9287a0857d354932df5532c05cf5afb7e5cb2d
absognety <vikasch.1994@gmail.com>
2020-02-12 19:18:48 +0530

    problem01 version 2 by using apply

---
+++ src/main/scala/ConstructorsandExceptions/problem01_v2.scala
@@ -0,0 +1,11 @@
+package ConstructorsandExceptions
+
+import codelisting.ExtensionException
+import codelisting.CodeListing._
+import codelisting.CodeListing
+object problem01_v2 {
+  def main(args: Array[String]): Unit = {
+    val reader = apply("/home/vikas/atomic-scala/src/main/scala/ConstructorsandExceptions/codelisting/CodeListing.scala")
+    reader.foreach(println)
+  }
+}
