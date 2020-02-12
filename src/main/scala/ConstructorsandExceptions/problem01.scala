e8f49734ee2546ffbc756ab380e760bfa28a9133
absognety <vikasch.1994@gmail.com>
2020-02-12 18:29:16 +0530

    Script to print each line in source scala file

--- src/main/scala/ConstructorsandExceptions/problem01.scala
+++ src/main/scala/ConstructorsandExceptions/problem01.scala
@@ -1,5 +1,11 @@
 package ConstructorsandExceptions
 
+import codelisting.ExtensionException
+import codelisting.CodeListing._
+import codelisting.CodeListing
 object problem01 {
-
+  def main(args: Array[String]): Unit = {
+    val reader = new CodeListing("/home/vikas/atomic-scala/src/main/scala/ConstructorsandExceptions/codelisting/CodeListing.scala")
+    reader.vec.foreach(println)
+  }
 }
