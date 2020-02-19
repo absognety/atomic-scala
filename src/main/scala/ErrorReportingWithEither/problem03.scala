2d092418124078c3651fae4492c1b6d5dc1b8e12
absognety <vikasch.1994@gmail.com>
2020-02-19 17:13:06 +0530

    use If-guard match statement

--- src/main/scala/ErrorReportingWithEither/problem03.scala
+++ src/main/scala/ErrorReportingWithEither/problem03.scala
@@ -1,5 +1,11 @@
 package ErrorReportingWithEither
 
 object problem03 {
-
+  val letters = ('a' to 'z') map {
+    case x => if (Seq('a','e','i','o','u').contains(x)) {Left(x)}
+    else {Right(x)}
+  }
+  def main(args: Array[String]): Unit = {
+    println (letters)
+  }
 }
