e82ed4785eb8bc8c89b75efacc20c1cd3430d133
absognety <vikasch.1994@gmail.com>
2020-02-21 17:01:59 +0530

    replace Try with Success for TryTransform.scala

--- src/main/scala/ConvertingExceptionsWithTry/solution01.scala
+++ src/main/scala/ConvertingExceptionsWithTry/solution01.scala
@@ -1,5 +1,21 @@
 package ConvertingExceptionsWithTry
+import util.{Try,Success}
+import ConvertingExceptionsWithTry.errors._
 
 object solution01 {
+  def f(n:Int) = Try(toss(n)).transform(
+    i => Success(s"$i Bob"), // Success
+    e => e match { // Failure
+      case Except1(why) => Success(why)
+      case Except2(n) => Success(n)
+      case Except3(msg, d) => Success(d)
+    }
+  ).get
 
+  def main(args: Array[String]): Unit = {
+    println (f(0))
+    println (f(1))
+    println (f(2))
+    println (f(3))
+  }
 }
